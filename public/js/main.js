bd06289358000ee6c716f579b8e97bf77f6b71ca
grif <grifanja88@gmail.com>
2019-05-25 16:30:08 +0300

    rename var

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 
 var newonew = document.getElementsByClassName('table-striped');
+
 if(newonew.length == 1){
     var idElement = newonew[0].id;
-    var elements  = document.getElementById(idElement);
-    elements.addEventListener('click',e => {
+    var list  = document.getElementById(idElement);
+    list.addEventListener('click',e => {
         if(e.target.className === 'btn btn-danger delete-task'){
             if(confirm('Are you sure nead delete?')){
                 const id = e.target.getAttribute('data-id');
